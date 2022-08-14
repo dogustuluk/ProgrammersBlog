3fab511cd9afdc4587005c7836b6e0c2b2eec8dd
dogustuluk <dogus.tuluk@gmail.com>
2022-08-14 14:40:52 +0300

    Entities class library bug fix and category class created

---
+++ ProgrammersBlog.Entities/Concrete/Category.cs
@@ -0,0 +1,16 @@
+﻿using ProgrammersBlog.Shared.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammersBlog.Entities.Concrete
+{
+    public class Category:EntityBase,IEntity
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public ICollection<Article> Articles { get; set; } //Web sitelerinde "post" olarak da gözükebilir. Kodlarken sorun olmaması için "Article" olarak tanımladık.
+    }
+}

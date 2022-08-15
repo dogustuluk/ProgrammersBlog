f13b5991a2edda600dea300fa546b5eeda003b14
dogustuluk <dogus.tuluk@gmail.com>
2022-08-15 22:20:18 +0300

    ICategoryService updated

--- ProgrammersBlog.Services/Abstract/ICategoryService.cs
+++ ProgrammersBlog.Services/Abstract/ICategoryService.cs
@@ -13,6 +13,7 @@ namespace ProgrammersBlog.Services.Abstract
     {
         Task<IDataResult<Category>> Get(int categoryId); //verilerimizi taşımak için oluşturduğumuz resul'ı veriyoruz.
         Task<IDataResult<IList<Category>>> GetAll();
+        Task<IDataResult<IList<Category>>> GetAllByNonDeleted();
         Task<IResult> Add(CategoryAddDto categoryAddDto, string createdByName); //Dto nedir -> data transfer object olarak geçer. Bunları view model olarak düşünebiliriz. bunlar bizim frontend kısmında sadece ihtiyacımız olan alanları içermektedir.
         Task<IResult> Update(CategoryUpdateDto categoryUpdateDto, string modifiedByName);
         Task<IResult> Delete(int categoryId);

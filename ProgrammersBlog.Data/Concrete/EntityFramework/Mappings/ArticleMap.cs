3c07ab84fd870fb36b9f8969f72a79ccca20495b
dogustuluk <dogus.tuluk@gmail.com>
2022-08-15 16:14:44 +0300

    Ef Core CLI add migration

--- ProgrammersBlog.Data/Concrete/EntityFramework/Mappings/ArticleMap.cs
+++ ProgrammersBlog.Data/Concrete/EntityFramework/Mappings/ArticleMap.cs
@@ -96,7 +96,7 @@ namespace ProgrammersBlog.Data.Concrete.EntityFramework.Mappings
                     Content = "Yaygın inancın tersine, Lorem Ipsum rastgele sözcüklerden oluşmaz. Kökleri M.Ö. 45 tarihinden bu yana klasik Latin edebiyatına kadar uzanan 2000 yıllık bir geçmişi vardır. Virginia'daki Hampden-Sydney College'dan Latince profesörü Richard McClintock, bir Lorem Ipsum pasajında geçen ve anlaşılması en güç sözcüklerden biri olan 'consectetur' sözcüğünün klasik edebiyattaki örneklerini incelediğinde kesin bir kaynağa ulaşmıştır. Lorm Ipsum, Çiçero tarafından M.Ö. 45 tarihinde kaleme alınan de Finibus Bonorum et Malorum (İyi ve Kötünün Uç Sınırları) eserinin 1.10.32 ve 1.10.33 sayılı bölümlerinden gelmektedir. Bu kitap, ahlak kuramı üzerine bir tezdir ve Rönesans döneminde çok popüler olmuştur. Lorem Ipsum pasajının ilk satırı olan Lorem ipsum dolor sit amet 1.10.32 sayılı bölümdeki bir satırdan gelmektedir.",
                     Thumbnail = "Default.jpg",
                     SeoDescription = "JavaScript Es2019 ve ES2020 Yenilikleri",
-                    SeoTags = "JavaScript, JS, JS ES2019, JS ES2020, ES2019, ES2020, JavaScript ES2019, JavaScript ES2020",
+                    SeoTags = "JavaScript",
                     SeoAuthor = "Doğuş Tuluk",
                     Date = DateTime.Now,
                     IsActive = true,

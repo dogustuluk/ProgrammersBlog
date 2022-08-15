6f78dcf71240576a6626eeb2eeafbdc8800b7c5e
dogustuluk <dogus.tuluk@gmail.com>
2022-08-15 16:46:39 +0300

    IDataResult and DataResult has been created

---
+++ ProgrammersBlog.Shared/Utilities/Results/Abstract/IDataResult.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammersBlog.Shared.Utilities.Results.Abstract
+{
+    public interface IDataResult<out T>:IResult //bu şekilde hem bir liste hem de tek bir data taşıyabiliriz.
+    {
+        public T Data { get;} //new DataResult<Category>(ResıltStatus.Success, category);
+        //eğer liste göndermek istersek -> new DataResult<IList<Category>>(ResultStatus.Success, categoryList); bunu yapabilmek için 'T'yi tek başına değil, out ile kullandık.
+    }
+}

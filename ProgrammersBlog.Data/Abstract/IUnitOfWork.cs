87c364426743c07090523a8f6cc624dac372cd22
dogustuluk <dogus.tuluk@gmail.com>
2022-08-15 14:04:12 +0300

    IUnitOfWork created

---
+++ ProgrammersBlog.Data/Abstract/IUnitOfWork.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammersBlog.Data.Abstract
+{
+    public interface IUnitOfWork:IDisposable
+    {
+        //UnitOfWork sayesinde tüm repository'lerimizi tek bir yerden kontrol etmemizi sağlıyor olucaz.
+        //burada EntityFramework yapısına bağımlı kalmıyor olucaz. ado.net ya da dapper yapısı da kullanıyor olsak bu UnitOfWork yapısını kullanabiliriz.
+        IArticleRepository Articles { get; } //unitofwork.Articles diyerek makalelere erişebiliyor olucaz.
+        ICategoryRepository Categories { get; }
+        ICommentRepository Comments { get; }
+        IRoleRepository Roles { get; }
+        IUserRepository Users { get; } //_unitOfWork.Categories.AddAsync(); -> kullanım örneği.
+        Task<int> SaveAsync(); //int olarak veriyoruz çünkü etkilenen kayıt sayısını almak isteyebiliriz.
+        /*örnek ->
+        _unitOfWork.Categories.AddAsync(user);
+        _unitOfWork.Users.AddAsync(user);
+        _unitOfWork.SaveAsync();
+        ->> herhangi birinde hata olursa tüm işlemler geri alınır.
+
+        */
+    }
+}

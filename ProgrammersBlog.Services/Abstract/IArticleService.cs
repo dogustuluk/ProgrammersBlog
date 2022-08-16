1ec4cc774b540a5a87ef3eaef983e4290fb2243c
dogustuluk <dogus.tuluk@gmail.com>
2022-08-16 17:35:56 +0300

    IArticleService interface has been created and related Dtos created ->(empty)

---
+++ ProgrammersBlog.Services/Abstract/IArticleService.cs
@@ -0,0 +1,24 @@
+﻿using ProgrammersBlog.Entities.Concrete;
+using ProgrammersBlog.Entities.Dtos;
+using ProgrammersBlog.Shared.Utilities.Results.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammersBlog.Services.Abstract
+{
+    public interface IArticleService
+    {
+        Task<IDataResult<ArticleDto>> Get(int articleId); //verilerimizi taşımak için oluşturduğumuz resul'ı veriyoruz.
+        Task<IDataResult<ArticleListDto>> GetAll();
+        Task<IDataResult<ArticleListDto>> GetAllByNonDeleted();
+        Task<IDataResult<ArticleListDto>> GetAllByNonDeletedAndActive();
+        Task<IDataResult<ArticleListDto>> GetAllByCategory(int categoryId);
+        Task<IResult> Add(ArticleAddDto articleAddDto, string createdByName); //Dto nedir -> data transfer object olarak geçer. Bunları view model olarak düşünebiliriz. bunlar bizim frontend kısmında sadece ihtiyacımız olan alanları içermektedir.
+        Task<IResult> Update(ArticleUpdateDto articleUpdateDto, string modifiedByName);
+        Task<IResult> Delete(int articleId, string modifiedByName);
+        Task<IResult> HardDelete(int articleId);
+    }
+}

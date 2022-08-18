cb4691edadb09f1c4b918904a28a1ccf597a1625
dogustuluk <dogus.tuluk@gmail.com>
2022-08-18 15:40:01 +0300

    ControllerExtensions created and CategoryController updated

--- ProgrammersBlog.Mvc/Areas/Admin/Controllers/CategoryController.cs
+++ ProgrammersBlog.Mvc/Areas/Admin/Controllers/CategoryController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ProgrammersBlog.Entities.Dtos;
+using ProgrammersBlog.Mvc.Areas.Admin.Models;
 using ProgrammersBlog.Services.Abstract;
+using ProgrammersBlog.Shared.Utilities.Extensions;
 using ProgrammersBlog.Shared.Utilities.Results.ComplexTypes;
 using System.Threading.Tasks;
 
@@ -21,9 +24,18 @@ namespace ProgrammersBlog.Mvc.Areas.Admin.Controllers
             return View(result.Data); 
            
         }
+        [HttpGet]
         public IActionResult Add()
         {
             return PartialView("_CategoryAddPartial");
+        } 
+        [HttpPost]
+        public async IActionResult Add(CategoryAddDto categoryAddDto)
+        {
+            var categoryAjaxModel = new CategoryAddAjaxViewModel
+            {
+                CategoryAddPartial = await this.RenderViewToStringAsync("_CategoryAddPartial", categoryAddDto),//partialView'i string'e çevirir ve model'i de beraber parse edecek.
+            };
         }
     }
 }

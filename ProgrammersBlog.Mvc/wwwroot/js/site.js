a13ad68d03770a41796d7ec659039f7f40aad4c3
dogustuluk <dogus.tuluk@gmail.com>
2022-08-23 18:09:31 +0300

    site.js created and added validation-summary on CategoryPartialAdd

---
+++ ProgrammersBlog.Mvc/wwwroot/js/site.js
@@ -0,0 +1,8 @@
+﻿function convertFirstLetterToUpperCase(text) {
+    return text.charAt(0).toUpperCase() + text.slice(1); //charAt fonksiyonu vereceğimiz index'teki harfi almamızı sağlar. 0 ile ilk harfi alır.
+    //slice metodu ise bir index ister ve string'in oradan alınmasını sağlar.
+}
+function convertToShortDate(dateString) {
+    const shortDate = new Date(dateString).toLocaleDateString('en-US'); //tr-TR -> ülkedeki format
+    return shortDate;
+}

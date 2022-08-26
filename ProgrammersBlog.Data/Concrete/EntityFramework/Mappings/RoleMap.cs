396837b28c8a56f5f023e5448bbe98ac469c58ef
dogustuluk <dogus.tuluk@gmail.com>
2022-08-26 16:40:41 +0300

    entities and map classes are created for Identity

--- ProgrammersBlog.Data/Concrete/EntityFramework/Mappings/RoleMap.cs
+++ ProgrammersBlog.Data/Concrete/EntityFramework/Mappings/RoleMap.cs
@@ -13,37 +13,32 @@ namespace ProgrammersBlog.Data.Concrete.EntityFramework.Mappings
     {
         public void Configure(EntityTypeBuilder<Role> builder)
         {
-            builder.HasKey(r => r.Id);
-            builder.Property(r => r.Id).ValueGeneratedOnAdd();
-            builder.Property(r => r.Name).IsRequired();
-            builder.Property(r => r.Name).HasMaxLength(30);
-            builder.Property(r => r.Description).IsRequired();
-            builder.Property(r => r.Description).HasMaxLength(250);
-            builder.Property(r => r.CreatedByName).IsRequired();
-            builder.Property(r => r.CreatedByName).HasMaxLength(50);
-            builder.Property(r => r.ModifiedByName).IsRequired();
-            builder.Property(r => r.ModifiedByName).HasMaxLength(50);
-            builder.Property(r => r.CreatedDate).IsRequired();
-            builder.Property(r => r.ModifiedDate).IsRequired();
-            builder.Property(r => r.IsActive).IsRequired();
-            builder.Property(r => r.IsDeleted).IsRequired();
-            builder.Property(r => r.Note).HasMaxLength(500);
-            builder.ToTable("Roles");
+                // Primary key
+                builder.HasKey(r => r.Id);
 
-            //HasData ile veri olup olmadığını kontrol ederiz.
-            builder.HasData(new Role
-            {
-                Id = 1, //elle tam veri tabanı oluştururken giriyorsak kendimiz tüm değerleri doldurmamız gerekiyor.
-                Name = "Admin",
-                Description = "Admin rolü tüm haklara sahiptir.",
-                IsActive = true,
-                IsDeleted = false,
-                CreatedByName = "InitialCreate", //InitialCreate -> ilk oluşturma işlemi anlamındadır. Kendimizin değil veri tabanının oluşturduğunu belirtiriz.
-                CreatedDate = DateTime.Now,
-                ModifiedByName = "InitialCreate",
-                ModifiedDate = DateTime.Now,
-                Note = "Admin rolüdür."
-            });
+                // Index for "normalized" role name to allow efficient lookups
+                builder.HasIndex(r => r.NormalizedName).HasDatabaseName("RoleNameIndex").IsUnique();
+
+                // Maps to the AspNetRoles table
+                builder.ToTable("AspNetRoles");
+
+                // A concurrency token for use with the optimistic concurrency checking
+                builder.Property(r => r.ConcurrencyStamp).IsConcurrencyToken();
+
+                // Limit the size of columns to use efficient database types
+                builder.Property(u => u.Name).HasMaxLength(100);
+                builder.Property(u => u.NormalizedName).HasMaxLength(100);
+
+                // The relationships between Role and other entity types
+                // Note that these relationships are configured with no navigation properties
+
+                // Each Role can have many entries in the UserRole join table
+                builder.HasMany<UserRole>().WithOne().HasForeignKey(ur => ur.RoleId).IsRequired();
+
+                // Each Role can have many associated RoleClaims
+                builder.HasMany<RoleClaim>().WithOne().HasForeignKey(rc => rc.RoleId).IsRequired();
+           
+
         }
     }
 }

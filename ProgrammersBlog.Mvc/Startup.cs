03479994b7c3716fc7f3751294181aded22a8c82
dogustuluk <dogus.tuluk@gmail.com>
2022-08-26 17:57:08 +0300

    cookie authentication configuration on startup.cs

--- ProgrammersBlog.Mvc/Startup.cs
+++ ProgrammersBlog.Mvc/Startup.cs
@@ -28,6 +28,21 @@ namespace ProgrammersBlog.Mvc
             services.AddSession();
             services.AddAutoMapper(typeof(CategoryProfile), typeof(ArticleProfile));//derlenme esnasýnda automapper'ýn buradaki sýnýflarý taramasýný saðlar.Profiles sýnýflarýný buluyor ve ekliyor.
             services.LoadMyServices();
+            services.ConfigureApplicationCookie(options =>
+            {
+                options.LoginPath = new PathString("/Admin/User/Login");//login için bu sayfaya yönlendiriyoruz.
+                options.LogoutPath = new PathString("/Admin/User/Logout");
+                options.Cookie = new CookieBuilder
+                {
+                    Name = "ProgrammersBlog",
+                    HttpOnly = true, //güvenlik için yapýlýr. js kodlarý ile cookie bilgilerinin gözükmemesi için
+                    SameSite = SameSiteMode.Strict, //güvenlik sebebiyle kullanýlýr. csrf zafiyeti, xsrf zaafiyeti ya da session riding olarak da adlandýrýlýr."siteler arasý istek sahtekarlýðý". Strict ifadesi ile kullanýcý bilgileri sadece kendi sitemiz üzerinden geldiðinde iþlenmesini saðlar.
+                    SecurePolicy = CookieSecurePolicy.SameAsRequest//gerçek uygulamalarda her daim "always" þeklinde kullanýlýr.
+                };
+                options.SlidingExpiration = true;//kullanýcý sitemize giriþ yaptýktan sonra kullanýcýya tanýnan zamaný ifade eder.
+                options.ExpireTimeSpan = TimeSpan.FromDays(7);//7 gün boyunca kullanýcýnýn tekrar giriþ yapmasý gerekmez.
+                options.AccessDeniedPath = new PathString("/Admin/User/AccessDenied");//yetkisi olmayan sayfalara girmeye çalýþtýðý zaman hangi sayfaya yönlendirileceðidir.
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

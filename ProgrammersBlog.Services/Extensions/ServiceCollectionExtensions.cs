59e43a4a364934367de346d9d7936d5d840f7240
dogustuluk <dogus.tuluk@gmail.com>
2022-08-26 17:27:26 +0300

    User Options created

--- ProgrammersBlog.Services/Extensions/ServiceCollectionExtensions.cs
+++ ProgrammersBlog.Services/Extensions/ServiceCollectionExtensions.cs
@@ -18,7 +18,20 @@ namespace ProgrammersBlog.Services.Extensions
         public static IServiceCollection LoadMyServices(this IServiceCollection serviceCollection)
         {
             serviceCollection.AddDbContext<ProgrammersBlogContext>();
-            serviceCollection.AddIdentity<User, Role>().AddEntityFrameworkStores<ProgrammersBlogContext>();
+            serviceCollection.AddIdentity<User, Role>(options =>
+            {
+                //User Password Options
+                options.Password.RequireDigit = false; //şifrede rakam bulunmasının ayarı.
+                options.Password.RequiredLength = 5;
+                options.Password.RequiredUniqueChars = 0;//özel karakterlerden kaç tane olması gerektiği(!,? gibi)
+                options.Password.RequireNonAlphanumeric = false;//@,!,?,$ işareti gibi özel karakterlerin kullanılmasını zorunlu kılıp kılmamanın ayarı.
+                options.Password.RequireLowercase = false;
+                options.Password.RequireUppercase = false;
+
+                //User Username and Email Options
+                options.User.AllowedUserNameCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+$#%"; //kullanıcı adı oluşturulurken kullanılan karakterler bütününü temsil eder. Özel karakterlerin bulunup bulunmadığını temsil eder.
+                options.User.RequireUniqueEmail = true;
+            }).AddEntityFrameworkStores<ProgrammersBlogContext>();
             serviceCollection.AddScoped<IUnitOfWork, UnitOfWork>();
             serviceCollection.AddScoped<ICategoryService, CategoryManager>();
             serviceCollection.AddScoped<IArticleService, ArticleManager>();

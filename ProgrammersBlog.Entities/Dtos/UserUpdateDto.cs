0caf804d0a41d34c9ce215c1496a2f4535af8aa5
dogustuluk <dogus.tuluk@gmail.com>
2022-08-27 14:17:08 +0300

    UserUpdateDto, _UserUpdatePartial created

---
+++ ProgrammersBlog.Entities/Dtos/UserUpdateDto.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammersBlog.Entities.Dtos
+{
+    public class UserUpdateDto
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [DisplayName("Kullanıcı Adı")]
+        [Required(ErrorMessage = "{0} boş geçilemez!")]
+        [MaxLength(50, ErrorMessage = "{0} {1} karakterden büyük olamaz!")]
+        [MinLength(3, ErrorMessage = "{0} {1} karakterden küçük olamaz!")]
+        public string UserName { get; set; }
+        [DisplayName("E-Posta Adresi")]
+        [Required(ErrorMessage = "{0} boş geçilemez!")]
+        [MaxLength(100, ErrorMessage = "{0} {1} karakterden büyük olamaz!")]
+        [MinLength(10, ErrorMessage = "{0} {1} karakterden küçük olamaz!")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+        public string Password { get; set; }
+        [DisplayName("Telefon Numarası")]
+        [Required(ErrorMessage = "{0} boş geçilemez!")]
+        [MaxLength(13, ErrorMessage = "{0} {1} karakterden büyük olamaz!")]
+        [MinLength(13, ErrorMessage = "{0} {1} karakterden küçük olamaz!")]
+        [DataType(DataType.PhoneNumber)]
+        public string PhoneNumber { get; set; }
+        [DisplayName("Resim Ekle")]
+        [DataType(DataType.Upload)]
+        public IFormFile PictureFile { get; set; }
+        [DisplayName("Resim")]
+        public string Picture { get; set; }//dosyanın adı saklanır sadece.
+    }
+}

5aa59f6e970fe84088c9409c730b5a91db9ff540
dogustuluk <dogus.tuluk@gmail.com>
2022-08-27 13:43:01 +0300

    usersTable refresh button

--- ProgrammersBlog.Mvc/wwwroot/js/userIndex.js
+++ ProgrammersBlog.Mvc/wwwroot/js/userIndex.js
@@ -21,56 +21,48 @@
                 action: function (e, dt, node, config) {
                     $.ajax({
                         type: 'GET', //kategori listesi almayı istediğimiz için type ->GET
-                        url: '/Admin/Category/GetAllCategories/',
+                        url: '/Admin/User/GetAllUsers/',
                         contentType: "application/json",//hangi formatta çalıştığımız veriyoruz. xml'de verebiliriz.
                         //ajax işleminde yapacağımız işlemelere geçiyoruz
                         beforeSend: function () { //ajax işlemini yapmadan önce yapmamız gereken işlemler.Örnek-> tablonun gizlenmesi ve spinner kısmının aktif edilmesii
                             //tabloyu gizle ve spinner'ı aktif et yani görünür yap
-                            $('#categoriesTable').hide();
+                            $('#usersTable').hide();
                             $('.spinner-border').show();
                         },
                         success: function (data) {
-                            const categoryListDto = jQuery.parseJSON(data);
-                            console.log(categoryListDto);
+                            const userListDto = jQuery.parseJSON(data);
+                            dataTable.clear();
+                            console.log(userListDto);
                             //ResultStatus durumunu kontrol etmeliyiz.
-                            if (categoryListDto.ResultStatus === 0) { //üç eşittir hem değerin aynı olup olmadığını hem de değerin
+                            if (userListDto.ResultStatus === 0) { //üç eşittir hem değerin aynı olup olmadığını hem de değerin
                                 //tipini kontrol eder. burada hem int hem de 0 olmasını istiyoruz. 0 ise başarılı
-                                let tableBody = ""; //sebebi -> foreach ile döndüğümüzde tr etiketlerimizin içerisine eklemektir.
-                                $.each(categoryListDto.Categories.$values, function (index, category) {
-                                    //ilk parametre olarak hangi değerler içerisinde dönüleceği sorulur.
-                                    tableBody += `
-                                             <tr>
-                                                <td>${category.Id}</td>
-                                                <td>${category.Name}</td>
-                                                <td>${category.Description}</td>
-                                                <td>${convertFirstLetterToUpperCase(category.IsActive.toString())}</td>
-                                                <td>${convertFirstLetterToUpperCase(category.IsDeleted.toString())}</td>
-                                                <td>${category.Note}</td>
-                                                <td>${convertToShortDate(category.CreatedDate)}</td>
-                                                <td>${category.CreatedByName}</td>
-                                                <td>${convertToShortDate(category.ModifiedDate)}</td>
-                                                <td>${category.ModifiedByName}</td>
-                                                <td>
-                                                <button class="btn btn-primary btn-sm btn-update" data-id="${category.Id}"><span class="fas fa-edit"></span></button>
-                                                <button class="btn btn-danger btn-sm btn-delete" data-id="${category.Id}"><span class="fas fa-minus-circle"></span></button>
-                                              </td>
-                                             </tr>`;
+                                $.each(userListDto.Users.$values, function (index, user) {
+                                    dataTable.row.add([
+                                        user.Id,
+                                        user.UserName,
+                                        user.Email,
+                                        user.PhoneNumber,
+                                        `<img src="/img/${user.Picture}" alt="${user.UserName}" style="max-height:50px; max-width:50px;">`,
+                                `       
+                                    <button class="btn btn-primary btn-sm btn-update" data-id="${user.Id}"><span class="fas fa-edit"></span></button>
+                                    <button class="btn btn-danger btn-sm btn-delete" data-id="${user.Id}"><span class="fas fa-minus-circle"></span> </button>
+                                `
+                                    ]);
                                 });
                                 //devamında ise gelen veri ile tablodaki verinin yer değiştirmesi gerekir.
-
-                                $('#categoriesTable > tbody').replaceWith(tableBody);
+                                dataTable.draw();
                                 $('.spinner-border').hide();
-                                $('#categoriesTable').fadeIn(1500);
+                                $('#usersTable').fadeIn(1500);
                             }
                             else {
-                                toastr.error(`${categoryListDto.Message}`, 'İşlem Başarısız!');
+                                toastr.error(`${userListDto.Message}`, 'İşlem Başarısız!');
 
                             }
                         },
                         error: function (err) {
                             console.log(err);
                             $('.spinner-border').hide();
-                            $('#categoriesTable').fadeIn(1000);
+                            $('#usersTable').fadeIn(1000);
                             toastr.error(`${err.responseText}`, 'Hata!');
 
                         }
@@ -150,10 +142,10 @@
                                 userAddAjaxModel.UserDto.User.Email,
                                 userAddAjaxModel.UserDto.User.PhoneNumber,
                                 `<img src="/img/${userAddAjaxModel.UserDto.User.Picture}" alt="${userAddAjaxModel.UserDto.User.UserName}" style="max-height:50px; max-width:50px;">`,
-                                `<td>
-                                    <button class="btn btn-primary btn-sm btn-update" data-id="userAddAjaxModel.UserDto.User.Id"><span class="fas fa-edit"></span></button>
-                                    <button class="btn btn-danger btn-sm btn-delete" data-id="userAddAjaxModel.UserDto.User.Id"><span class="fas fa-minus-circle"></span> </button>
-                                </td>`
+                                `
+                                    <button class="btn btn-primary btn-sm btn-update" data-id="${userAddAjaxModel.UserDto.User.Id}"><span class="fas fa-edit"></span></button>
+                                    <button class="btn btn-danger btn-sm btn-delete" data-id="${userAddAjaxModel.UserDto.User.Id}"><span class="fas fa-minus-circle"></span> </button>
+                                `
                             ]).draw();
                             //toastr start
                             toastr.success(`${userAddAjaxModel.UserDto.Message}`, 'Başarılı İşlem!');

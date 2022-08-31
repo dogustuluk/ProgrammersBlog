8fb85fa62027a2ebdaf768f99fe215d67129a07d
dogustuluk <dogus.tuluk@gmail.com>
2022-08-31 15:10:43 +0300

    project updated, fixed and BreadCrumb added for other page.

--- ProgrammersBlog.Mvc/Areas/Admin/Controllers/HomeController.cs
+++ ProgrammersBlog.Mvc/Areas/Admin/Controllers/HomeController.cs
@@ -28,9 +28,9 @@ namespace ProgrammersBlog.Mvc.Areas.Admin.Controllers
         }
         public async Task <IActionResult> Index()
         {
-            var categoriesCountResult = await _categoryService.CountByIsDeleted();
-            var articlesCountResult = await _articleService.CountByIsDeleted();
-            var commentsCountResult = await _commentService.CountByIsDeleted();
+            var categoriesCountResult = await _categoryService.CountByNonDeleted();
+            var articlesCountResult = await _articleService.CountByNonDeleted();
+            var commentsCountResult = await _commentService.CountByNonDeleted();
             var userCount = await _userManager.Users.CountAsync();
             var articlesResult = await _articleService.GetAll();
             if (categoriesCountResult.ResultStatus == ResultStatus.Success && articlesCountResult.ResultStatus == ResultStatus.Success && commentsCountResult.ResultStatus == ResultStatus.Success && userCount > -1 && articlesResult.ResultStatus == ResultStatus.Success)

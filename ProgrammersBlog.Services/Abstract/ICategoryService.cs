36abb8c6e9538aac70b8bdf4c653d7eb248bd27a
dogustuluk <dogus.tuluk@gmail.com>
2022-08-31 16:10:32 +0300

    xml comment added

--- ProgrammersBlog.Services/Abstract/ICategoryService.cs
+++ ProgrammersBlog.Services/Abstract/ICategoryService.cs
@@ -12,10 +12,21 @@ namespace ProgrammersBlog.Services.Abstract
     public interface ICategoryService
     {
         Task<IDataResult<CategoryDto>> GetAsync(int categoryId); //verilerimizi taşımak için oluşturduğumuz result'ı veriyoruz.
+        /// <summary>
+        /// Verilen ID parametresine ait kategorinin CategoryUpdateDto temsilini geriye döner.
+        /// </summary>
+        /// <param name="categoryId">0'dan büyük integer bir ID değeri.</param>
+        /// <returns>Asenkron bir operasyon ile Task olarak işlem sonucunu DataResult tipinde geriye döner.</returns>
         Task<IDataResult<CategoryUpdateDto>> GetCategoryUpdateDtoAsync(int categoryId); //categoryId alacak fakat alınan bu parametreyi içeride işleyip categoryUpdateDto'ya dönüştürülüp o şekilde return edilecek.
         Task<IDataResult<CategoryListDto>> GetAllAsync();
         Task<IDataResult<CategoryListDto>> GetAllByNonDeletedAsync();
         Task<IDataResult<CategoryListDto>> GetAllByNonDeletedAndActiveAsync();
+        /// <summary>
+        /// Verilen CategoryAddDto ve CreatedByName parametrelerine ait bilgiler ile yeni bir Category ekler.
+        /// </summary>
+        /// <param name="categoryAddDto">categoryAddDto tipinde eklenecek kategori bilgileri.</param>
+        /// <param name="createdByName">string tipinde kullanıcının kullanıcı adı.</param>
+        /// <returns>Asenkron bir operasyon ile Task olarak bizlere ekleme işleminin sonucunu DataResult tipinde döner.</returns>
         Task<IDataResult<CategoryDto>> AddAsync(CategoryAddDto categoryAddDto, string createdByName); //Dto nedir -> data transfer object olarak geçer. Bunları view model olarak düşünebiliriz. bunlar bizim frontend kısmında sadece ihtiyacımız olan alanları içermektedir.
         Task<IDataResult<CategoryDto>> UpdateAsync(CategoryUpdateDto categoryUpdateDto, string modifiedByName);
         Task<IDataResult<CategoryDto>> DeleteAsync(int categoryId, string modifiedByName);

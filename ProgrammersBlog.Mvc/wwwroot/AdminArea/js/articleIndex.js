6c3d39598a80071afe8dbed1e31ec79af3c0565a
dogustuluk <dogus.tuluk@gmail.com>
2022-09-01 13:23:39 +0300

    Article Add and styles area(RenderSection) created

--- ProgrammersBlog.Mvc/wwwroot/AdminArea/js/articleIndex.js
+++ ProgrammersBlog.Mvc/wwwroot/AdminArea/js/articleIndex.js
@@ -12,6 +12,9 @@
                 },
                 className: 'btn btn-success',
                 action: function (e, dt, node, config) {
+                    let url = window.location.href;
+                    url = url.replace("/Index", "");
+                    window.open(`${url}/Add`, "_self");//self ile aynı sayfa içerisinde bu url'in açılmasını sağlar
 
                 }
             },

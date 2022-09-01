9ac0e2c39b308830b06c239405182f076e549844
dogustuluk <dogus.tuluk@gmail.com>
2022-09-01 14:19:55 +0300

    added Trumbowyg librar for Article Add page

---
+++ ProgrammersBlog.Mvc/wwwroot/AdminArea/js/articleAdd.js
@@ -0,0 +1,36 @@
+﻿$(document).ready(function () {
+    $('#text-editor').trumbowyg({
+        lang:['tr'],
+        btns: [
+            ['viewHTML'],
+            ['undo', 'redo'], // Only supported in Blink browsers
+            ['formatting'],
+            ['strong', 'em', 'del'],
+            ['superscript', 'subscript'],
+            ['link'],
+            ['insertImage'],
+            ['justifyLeft', 'justifyCenter', 'justifyRight', 'justifyFull'],
+            ['unorderedList', 'orderedList'],
+            ['horizontalRule'],
+            ['removeformat'],
+            ['fullscreen'],
+            ['foreColor', 'backColor'],
+            ['emoji'],
+            ['fontfamily'],
+            ['fontsize']
+        ],
+        plugins: {
+            colors: {
+                foreColorList: [
+                    'ff0000', '00ff00', '0000ff', '42855B', 'FF4A4A', '472D2D','774360'
+                ],
+                backColorList: [
+                    '000', '333', '555'
+                ],
+                displayAsList: false
+            }
+        }
+        
+    });
+    
+});

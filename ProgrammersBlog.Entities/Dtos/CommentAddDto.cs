70b419b9bc169a74b0ebe598e868d686957b01ce
dogustuluk <dogus.tuluk@gmail.com>
2022-09-03 13:42:19 +0300

    CommentDto, CommentAddDto, CommentListDto, CommentUpdateDto created

---
+++ ProgrammersBlog.Entities/Dtos/CommentAddDto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammersBlog.Entities.Dtos
+{
+    public class CommentAddDto
+    {
+        [DisplayName("Yorum")]
+        [Required(ErrorMessage ="{0} boş geçilmemelidir!")]
+        [MaxLength(1000, ErrorMessage ="{0} {1} karakterden büyük olmamalıdır!")]
+        [MinLength(2, ErrorMessage ="{0} {1} karakterden küçük olmamalıdır!")]
+        public string Text { get; set; }
+        [DisplayName("Adınız")]
+        [Required(ErrorMessage ="{0} boş geçilmemelidir!")]
+        [MaxLength (50, ErrorMessage ="{0} {1} karakterden büyük olmamalıdır!")]
+        [MinLength (2, ErrorMessage ="{0} {1} karakterden küçük olmamalıdır!")]
+        public string CreatedByName { get; set; }
+    }
+}

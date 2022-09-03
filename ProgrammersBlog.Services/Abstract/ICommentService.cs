5786a87b41f7dc9493b03df3974a1eac962737e9
dogustuluk <dogus.tuluk@gmail.com>
2022-09-03 13:48:04 +0300

    created ICommentService

--- ProgrammersBlog.Services/Abstract/ICommentService.cs
+++ ProgrammersBlog.Services/Abstract/ICommentService.cs
@@ -1,4 +1,5 @@
-﻿using ProgrammersBlog.Shared.Utilities.Results.Abstract;
+﻿using ProgrammersBlog.Entities.Dtos;
+using ProgrammersBlog.Shared.Utilities.Results.Abstract;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +10,16 @@ namespace ProgrammersBlog.Services.Abstract
 {
     public interface ICommentService
     {
+        Task<IDataResult<CommentDto>> GetAsync(int commentId);
+        Task<IDataResult<CommentUpdateDto>> GetCommentUpdateDtoAsync(int commentId);
+        Task<IDataResult<CommentListDto>> GetAllAsync();
+        Task<IDataResult<CommentListDto>> GetAllByDeletedAsync();
+        Task<IDataResult<CommentListDto>> GetAllByNonDeletedAsync();
+        Task<IDataResult<CommentListDto>> GetAllBtNonDeletedAndActiveAsync();
+        Task<IDataResult<CommentDto>> AddAsync(CommentAddDto commentAddDto);
+        Task<IDataResult<CommentDto>> UpdateAsync(CommentUpdateDto commentUpdateDto, string modifiedByName);
+        Task<IDataResult<CommentDto>> DeleteAsync(int commentId, string modifiedByName);
+        Task<IResult> HardDeleteAsync(int commentId);
         Task<IDataResult<int>> CountAsync();
         Task<IDataResult<int>> CountByNonDeletedAsync();
 

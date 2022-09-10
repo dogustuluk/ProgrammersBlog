a092521e198ee609837c48384ff57326329a599c
dogustuluk <dogus.tuluk@gmail.com>
2022-09-10 11:00:19 +0300

    created MvcErrorModel and MvcExceptionFilter

---
+++ ProgrammersBlog.Mvc/Filters/MvcExceptionFilter.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.Extensions.Hosting;
+using ProgrammersBlog.Shared.Entities.Concrete;
+
+namespace ProgrammersBlog.Mvc.Filters
+{
+    public class MvcExceptionFilter : IExceptionFilter
+    {
+        private readonly IHostEnvironment _environment;
+        private readonly IModelMetadataProvider _metadataProvider;
+        public MvcExceptionFilter(IHostEnvironment environment, IModelMetadataProvider metadataProvider)
+        {
+            _environment = environment;
+            _metadataProvider = metadataProvider;
+        }
+
+        public void OnException(ExceptionContext context)
+        {
+            //hangi ortamda olduğumuzun kontrolünü yapıyoruz.
+            if (_environment.IsDevelopment())//geliştirme ortamı. gerçek ortam -> isProduction
+            {
+                context.ExceptionHandled = true;//hata kısmını biz ele almış olduğumuz için true veriyoruz.
+                var mvcErrorModel = new MvcErrorModel
+                {
+                    Message = $"Üzgünüz, işleminiz sırasında beklenmedik bir hata oluştu. Sorunu en kısa sürede çözeceğiz."
+                };
+                var result = new ViewResult { ViewName = "Error" };
+                result.StatusCode = 500;//500 kodu -> internal server error
+                result.ViewData = new ViewDataDictionary(_metadataProvider, context.ModelState);
+                result.ViewData.Add("MvcErrorModel", mvcErrorModel);
+                context.Result = result;
+            }
+        }
+    }
+}
